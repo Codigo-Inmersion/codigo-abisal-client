73ab1c623df4249413b06a50d949069a51b10ca5
olgararo <olgararo1402@gmail.com>
2025-10-05 00:01:20 +0200

    feature: add  background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import LiquidEther from "./components/layout/LiquidEther/LiquidEther";
+import Background from "./components/layout/Background/background";
+
 
 function App() {
   return (
     <div className="app-container">
-      <div className="liquid-ether-background">
-        <LiquidEther
-          colors={["#5227FF", "#A4DEFB", "#A4FBFB"]}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
-        />
-      </div>
+      <Background/>
 
       {/* Contenido de la app */}
       <div className="app-content">
---
+++ src/components/layout/Background/Background.jsx
@@ -0,0 +1,26 @@
+
+import LiquidEther from "../../layout/LiquidEther/LiquidEther"
+
+export default function Background() {
+  return (
+    <div className="liquid-ether-background">
+      <LiquidEther
+        colors={["#5227FF", "#A4DEFB", "#A4FBFB"]}
+        mouseForce={20}
+        cursorSize={100}
+        isViscous={false}
+        viscous={30}
+        iterationsViscous={32}
+        iterationsPoisson={32}
+        resolution={0.5}
+        isBounce={false}
+        autoDemo={true}
+        autoSpeed={0.5}
+        autoIntensity={2.2}
+        takeoverDuration={0.25}
+        autoResumeDelay={3000}
+        autoRampDuration={0.6}
+      />
+    </div>
+  );
+}
