bfb3064a262192a21637e091af7d1232fdb9ab1d
MarianaMH1195 <morenomariana03@gmail.com>
2025-10-14 11:17:47 +0200

    update footer style

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="footer-container">
         <div className="footer-copy">
           <p>
-            © {new Date().getFullYear()} Código Abisal. Proyecto hecho en{" "}
+            © {new Date().getFullYear()} Código Abisal | Proyecto hecho en{" "}
             <span className="footer-highlight">Factoría F5</span>.
           </p>
         </div>
