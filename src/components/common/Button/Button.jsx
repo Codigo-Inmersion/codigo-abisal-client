f9bca9871dda58849add4aada5ebf4cf23b08be9
olgararo <olgararo1402@gmail.com>
2025-10-15 19:15:52 +0200

    style: fix gap buttons

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,18 +1,26 @@
 // src/components/common/Button/Button.jsx
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Button.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
 
-const Button = ({ children, onClick, type = 'button', variant = 'primary' }) => {
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  className = "",
+  disabled = false,
+}) => {
   // `variant` nos permite tener diferentes estilos de botón en el futuro (ej: 'primary', 'secondary')
-  const buttonClass = `btn ${variant}`;
+  const buttonClass = `btn ${variant} ${className}`.trim();
 
   return (
     <button
       type={type}
       className={buttonClass}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
@@ -22,8 +30,10 @@ const Button = ({ children, onClick, type = 'button', variant = 'primary' }) => 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
-  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
   variant: PropTypes.string,
+  className: PropTypes.string,
+  disabled: PropTypes.bool   
 };
 
 export default Button;
